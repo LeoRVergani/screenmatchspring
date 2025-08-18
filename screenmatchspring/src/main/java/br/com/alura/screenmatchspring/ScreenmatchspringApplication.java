55be5113f141fd54ad6686ab882d0817b8404c56
leorvergani <vergani@outlook.com.br>
2025-08-17 22:44:16 -0300

    :tada: update

---
+++ screenmatchspring/src/main/java/br/com/alura/screenmatchspring/ScreenmatchspringApplication.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatchspring;
+
+import br.com.alura.screenmatchspring.model.DadosSerie;
+import br.com.alura.screenmatchspring.service.ConsumoApi;
+import br.com.alura.screenmatchspring.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchspringApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchspringApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=60c99a64");
+        System.out.println(json);
+
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+
+    }
+}
